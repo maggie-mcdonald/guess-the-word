aa0e0fc6a4bc41bf00392482904c606991f8cf17
Maggie McDonald <113684788+maggie-mcdonald@users.noreply.github.com>
2023-01-12 05:40:17 -0500

    Coded the initial player input

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const guessedLetters = document.querySelector(".guessed-letters"); //ul guessed letters
+const guessedLettersElement = document.querySelector(".guessed-letters"); //ul guessed letters
 const guessButton = document.querySelector(".guess"); // button w./guess text
 const letterInput = document.querySelector(".letter"); //text input
 const wordInProgress = document.querySelector(".word-in-progress"); // display word in progress
@@ -7,6 +7,7 @@ const remainingSpan = document.querySelector(".remaining span"); // span selecto
 const message = document.querySelector(".message"); //where messages appear after guessed letter
 const playAgainButton = document.querySelector(".play-again"); //hidden button appears to prompt another round 
 const word = "magnolia";
+const guessedLetters = [];
 
 const placeholder = function (word){
     const placeholderLetters = [];
@@ -21,7 +22,35 @@ placeholder(word);
 
 guessButton.addEventListener("click", function (e) {
     e.preventDefault();
+    message.innerText = "";
     const guess = letterInput.value;
     console.log(guess);
+    const goodGuess = validateInput(guess);
+    if (goodGuess) {
+        makeGuess(guess);
+    }
     letterInput.value = "";
     });
+
+const validateInput = function (input) {
+    const acceptedLetter = /[a-zA-Z]/;
+    if (input.length === 0) {
+        message.innerText = "Please enter a letter";
+    } else if (input.length > 1) {
+        message.innerText = "Please enter a single letter";
+    } else if (!input.match(acceptedLetter)) {
+        message.innerText = "Please enter a letter from A to Z."
+    } else {
+        return input;
+    }
+};
+
+const makeGuess = function (guess) {
+    guess= guess.toUpperCase();
+    if ( guessedLetters.includes(guess)) {
+        message.innerText = "You have already guessed that letter, silly. Try again.";
+    } else {
+        guessedLetters.push(guess);
+        console.log(guessedLetters);
+    }
+}
